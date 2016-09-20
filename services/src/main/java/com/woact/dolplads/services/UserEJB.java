2d567ef605d848964307fa96fe64e4c5ae04dcbb
dolps <dolplads@gmail.com>
2016-09-20 12:07:47 +0200

    think it works now

--- services/src/main/java/com/woact/dolplads/services/UserEJB.java
+++ services/src/main/java/com/woact/dolplads/services/UserEJB.java
@@ -3,10 +3,12 @@ package com.woact.dolplads.services;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.runner.RunWith;
 
+import javax.ejb.Stateless;
+
 /**
  * Created by dolplads on 20/09/16.
  */
-
+@Stateless
 public class UserEJB {
     public String sayHello() {
         return "hello";
